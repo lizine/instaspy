2fb87c8e8ab1fb17c5d5f917844ffc5b4e8780b0
Teemu Sarapisto <tsarapisto@topcon.com>
2015-01-31 21:17:30 +0200

    /coordinates/ path should now actually return stuff

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 
 var express = require('express'),
     bodyParser = require('body-parser'),
+    request = require('request'),
     app = express();
+
 
 
 var pub = __dirname + '/../public';
@@ -15,8 +17,33 @@ var server = app.listen(port, function() {
     console.log('listening');
 });
 
+
+var baseUrl = "https://maps.googleapis.com/maps/api/geocode/json?address=";
+
+
 app.post('/coordinates/', function(request, response) {
     console.log(request.body); 
+
+    var apiUrl = baseUrl + request.body.address + "&key=" + apiKey;
+    console.log(apiUrl); 
+    
+
+
+    request(apiUrl, function(error, response, body) {
+        var formattedAddress = body.results[0].formatted_address;
+        var location = body.results[0].geometry.location;
+
+        var obj = {
+           address: formattedAddress,
+           location: location
+        };
+        
+        console.log('sending');
+        console.log(obj);
+        response.json(obj);
+        response.end();
+        
+    });
     response.end();
 });
 
