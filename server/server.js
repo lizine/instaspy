36eb794b1cc61f771d410affd07b7212f0ec45b6
Teemu Sarapisto <tsarapisto@topcon.com>
2015-01-31 19:50:15 +0200

    Added a super simple server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    app = express();
+
+
+var pub = __dirname + '/../public';
+app.use(express.static(pub));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+var port = 4000;
+
+var server = app.listen(port, function() {
+    console.log('listening');
+});
+
+
+
