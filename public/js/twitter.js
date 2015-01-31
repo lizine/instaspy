e4a1cb7bda8bc360a50f9a40bb53f8a1830a7e88
Nini <nini@localhost.localdomain>
2015-01-31 19:32:26 +0200

    tweet search

---
+++ public/js/twitter.js
@@ -0,0 +1,11 @@
+angular.module('Twitter', ['ngResource']);
+
+function TwitterCtrl($scope, $resource){
+ $scope.twitter = $resource('http://search.twitter.com/:action',
+ {action:'search.json', q:'angularjs', callback:'JSON_CALLBACK'},
+ {get:{method:'JSONP'}});
+
+ $scope.searchTweet = function(){
+   $scope.twitterResult = $scope.twitter.get({q:$scope.searchTerm});
+  };
+}
