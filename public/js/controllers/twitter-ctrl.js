6724e98db221c09f0820470b920ef752cfdfa774
Nini <nini@localhost.localdomain>
2015-02-09 21:20:19 +0200

    its now possible for user to search pictures by longitude and latitude

--- public/js/controllers/twitter-ctrl.js
+++ public/js/controllers/twitter-ctrl.js
@@ -1,12 +1,18 @@
+
+
   app.factory("InstagramAPI", ['$http', function($http) {
-	var client_id = '5005fb61f69a4b44ad53ab6bc2662431';
+  
+
     return {
-      fetchPopular: function(callback){
+      fetchPopular: function(lon,lat, callback){
         var endpoint = "https://api.instagram.com/v1/media/search?";
+        var lat = lat;
+        var lon = lon;
         
-        endpoint += "&client_id=" + client_id;
-        endpoint +="&lat=60.19524"
-        endpoint +="&lng=24.95888"
+        
+        endpoint += "&client_id=5005fb61f69a4b44ad53ab6bc2662431";
+        endpoint +="&lat=" + lat;
+        endpoint +="&lng=" + lon;
         endpoint +="&distance=100"
         endpoint += "&callback=JSON_CALLBACK";
         $http.jsonp(endpoint).success(function(response){
@@ -16,13 +22,24 @@
     }
   }]);
 
-app.controller('TwitterCtrl', function ($scope, InstagramAPI) {
-	$scope.data = {};
 
-	InstagramAPI.fetchPopular(function(data){
+app.controller('TwitterCtrl', function ($scope, InstagramAPI) {
+	
+
+  
+
+  $scope.searchTweet = function(lon,lat){
+  $scope.data = {};
+
+
+	InstagramAPI.fetchPopular(lon, lat, function(data){
 		$scope.pics = data;
+    
+    
 
 	});  
+};
+
 });
 
 
