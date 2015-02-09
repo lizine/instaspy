438b0f0bcfb762ad8fc52474f1c64b9c615b0d7d
Nini <nini@localhost.localdomain>
2015-02-09 19:29:59 +0200

    testing API search based on latitude and longitude

--- public/js/controllers/twitter-ctrl.js
+++ public/js/controllers/twitter-ctrl.js
@@ -2,9 +2,12 @@
 	var client_id = '5005fb61f69a4b44ad53ab6bc2662431';
     return {
       fetchPopular: function(callback){
-        var endpoint = "https://api.instagram.com/v1/media/popular";
-        endpoint += "?count=15";
+        var endpoint = "https://api.instagram.com/v1/media/search?";
+        
         endpoint += "&client_id=" + client_id;
+        endpoint +="&lat=60.19524"
+        endpoint +="&lng=24.95888"
+        endpoint +="&distance=100"
         endpoint += "&callback=JSON_CALLBACK";
         $http.jsonp(endpoint).success(function(response){
           callback(response.data);
@@ -15,6 +18,7 @@
 
 app.controller('TwitterCtrl', function ($scope, InstagramAPI) {
 	$scope.data = {};
+
 	InstagramAPI.fetchPopular(function(data){
 		$scope.pics = data;
 
